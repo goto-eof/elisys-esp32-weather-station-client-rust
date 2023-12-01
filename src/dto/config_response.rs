ec66345202cf73a686ad384ca9d2bb651928e724
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-02 00:00:44 +0100

    refactor

--- src/dto/config_response.rs
+++ src/dto/config_response.rs
@@ -6,11 +6,6 @@ pub struct Configuration {
     pub alert_endpoint: String,
     #[serde(rename = "iAmAliveEndpoint")]
     pub i_am_alive_endpoint: String,
-    // #[serde(rename = "iAmAliveIntervalSeconds")]
-    // pub i_am_alive_interval_seconds: u64,
-    // pub crontab: String,
-    // #[serde(rename = "timezoneOffsetSec")]
-    // pub timezone_offset: i32,
     #[serde(rename = "temperatureSensorUnitOfMeasure")]
     pub temperature_sensor_unit_of_measure: String,
     #[serde(rename = "weatherSensorSupplyIntervalSeconds")]
